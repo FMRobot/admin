dee68c5848a9a05565cbdaacca4fd6a120c9814e
Anton Nemtsev <thesilentimp@gmail.com>
2016-01-16 16:41:39 +0200

    deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,29 +1,33 @@
 #!/usr/bin/env bash
 echo "Starting deployment"
 echo "Creating archive"
-echo "\n"
+echo ""
 
-mkdir build$TRAVIS_COMMIT
+mkdir bxuild$TRAVIS_COMMIT
 if [ $? != 0 ]; then
-    echo 'Can not create folder\n'
+    echo $?
+    echo 'Can not create folder'
     quit
 fi
 
-mv -v ./* ./build$TRAVIS_COMMIT
+mv -vrf ./!(build$TRAVIS_COMMIT) ./build$TRAVIS_COMMIT
 if [ $? != 0 ]; then
-    echo 'Can not move files\n'
-    quit
+    echo $?
+    echo 'Can not move files'
+    exit
 fi
 
 tar -czf package.tgz build$TRAVIS_COMMIT
 if [ $? != 0 ]; then
-    echo 'Can not create archive\n'
-    quit
+    echo $?
+    echo 'Can not create archive'
+    exit
 fi
 
 export SSHPASS=$SSH_PASS
 sshpass -e scp package.tgz $SSH_USER@$SSH_IP:$WEB_PATH
 if [ $? != 0 ]; then
-    echo 'Can not upload files\n'
-    quit
+    echo $?
+    echo 'Can not upload files'
+    exit
 fi
