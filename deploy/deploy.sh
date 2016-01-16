58061760f061707426105ced0e64fd083ae51c99
Anton Nemtsev <thesilentimp@gmail.com>
2016-01-16 17:48:08 +0200

    deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -34,6 +34,6 @@ if [ $? != 0 ]; then
     exit
 fi
 
-npm = "type cd; cd '$(WEB_PATH)'; echo $PWD; whoami; tar -xvzf ./package.tgz; rm ./package.tgz; source ./build_$TRAVIS_COMMIT/deploy/deploy.sh"
+npm = "type cd; cd '$(echo $WEB_PATH)'; echo $PWD; whoami; tar -xvzf ./package.tgz; rm ./package.tgz; source ./build_$TRAVIS_COMMIT/deploy/deploy.sh"
 
 sshpass -e ssh $SSH_USER@$SSH_IP
