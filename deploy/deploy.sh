c2a33d46bc0dc78eb6f6471aeafed1686854ed86
Anton Nemtsev <thesilentimp@gmail.com>
2016-01-17 11:53:18 +0200

    silent deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,45 +5,14 @@ export SSHPASS=$SSH_PASS
 export ARCH_NAME=frontend.package.tgz
 export SYMLINK_NAME=admin.frontend
 
-echo "Starting deploy"
-
 mkdir $RESULT_DIR
-if [ $? != 0 ]; then
-    echo $?
-    echo 'Can not create folder'
-    quit
-fi
-
-shopt -s extglob
 mv -vf ./build/* ./$RESULT_DIR
-if [ $? != 0 ]; then
-    echo $?
-    echo 'Can not move files'
-    exit
-fi
-
 tar -czf $ARCH_NAME $RESULT_DIR
-if [ $? != 0 ]; then
-    echo $?
-    echo 'Can not create archive'
-    exit
-fi
-
 sshpass -e scp -C -o StrictHostKeyChecking=no $ARCH_NAME $SSH_USER@$SSH_IP:$WEB_PATH
-
-if [ $? != 0 ]; then
-    echo $?
-    echo 'Can not upload files'
-    exit
-fi
-
 sshpass -e ssh -C $SSH_USER@$SSH_IP << EOF
 cd $WEB_PATH;
-echo 'Extracting';
 tar -xzf ./$ARCH_NAME -C ./;
-echo 'Cleaning';
 rm ./$ARCH_NAME;
-echo 'Linking';
 rm -dRf $SYMLINK_NAME
 ln -ds $RESULT_DIR ./$SYMLINK_NAME
 EOF
