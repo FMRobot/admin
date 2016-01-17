11214ed84277536c20b7e395fa2b951ac6e6108e
Anton Nemtsev <thesilentimp@gmail.com>
2016-01-17 14:11:40 +0200

    muted deploy with ssh compression

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,65 +1,31 @@
 #!/usr/bin/env bash
 
-export RESULT_DIR=admin.server.$TRAVIS_BUILD_NUMBER
-export SSHPASS=$SSH_PASS
+export RESULT_DIR=admin.server.${TRAVIS_BUILD_NUMBER}
+export SSHPASS=${SSH_PASS}
 export ARCH_NAME=server.package.tgz
 export SYMLINK_NAME=admin.server
 export PROCESS_NAME=admin.server
 
-echo "Starting deploy"
-
-mkdir $RESULT_DIR
-if [ $? != 0 ]; then
-    echo $?
-    echo 'Can not create folder'
-    quit
-fi
-
+mkdir ${RESULT_DIR}
 shopt -s extglob
-mv -vf !($RESULT_DIR) ./$RESULT_DIR
-if [ $? != 0 ]; then
-    echo $?
-    echo 'Can not move files'
-    exit
-fi
-
-tar -czf $ARCH_NAME $RESULT_DIR
-if [ $? != 0 ]; then
-    echo $?
-    echo 'Can not create archive'
-    exit
-fi
-
-sshpass -e scp -o StrictHostKeyChecking=no $ARCH_NAME $SSH_USER@$SSH_IP:$WEB_PATH
-
-if [ $? != 0 ]; then
-    echo $?
-    echo 'Can not upload files'
-    exit
-fi
-
-sshpass -e ssh $SSH_USER@$SSH_IP << EOF
-cd $WEB_PATH;
-echo 'Extracting';
-tar -xzf ./$ARCH_NAME -C ./;
-echo 'Cleaning';
-rm ./$ARCH_NAME;
-echo 'Deploying';
+mv -f !(${RESULT_DIR}) ./${RESULT_DIR}
+tar -czf ${ARCH_NAME} ${RESULT_DIR}
+sshpass -e scp -C -o StrictHostKeyChecking=no ${ARCH_NAME} ${SSH_USER}@${SSH_IP}:${WEB_PATH}
+sshpass -e ssh -C ${SSH_USER}@${SSH_IP} << EOF
+cd ${WEB_PATH};
+tar -xzf ./${ARCH_NAME} -C ./;
+rm ./${ARCH_NAME};
 if [ ! -f ".env" ]; then
-    echo APP_SECRET=$APP_SECRET >> .env;
-    echo APP_PUBLIC=$APP_PUBLIC >> .env;
+    echo APP_SECRET=${APP_SECRET} >> .env;
+    echo APP_PUBLIC=${APP_PUBLIC} >> .env;
 fi
-cd $RESULT_DIR;
+cd ${RESULT_DIR};
 npm install;
-if [ $? != 0 ]; then
-    echo 'Npm install failed';
-    quit;
-fi
 cd ..
-rm -dRf $SYMLINK_NAME
-ln -ds $RESULT_DIR ./$SYMLINK_NAME
-cd ./$SYMLINK_NAME
-pm2 stop $PROCESS_NAME
-pm2 delete $PROCESS_NAME
-pm2 start server.js --name="$PROCESS_NAME"
+rm -dRf ${SYMLINK_NAME}
+ln -ds ${RESULT_DIR} ./${SYMLINK_NAME}
+cd ./${SYMLINK_NAME}
+pm2 stop ${PROCESS_NAME}
+pm2 delete ${PROCESS_NAME}
+pm2 start server.js --name="${PROCESS_NAME}"
 EOF
