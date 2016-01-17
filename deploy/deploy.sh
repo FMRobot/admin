9bfe22c49b2e5c285c74b04a763043e4291ce9f2
Anton Nemtsev <thesilentimp@gmail.com>
2016-01-17 12:49:12 +0200

    fixed warnings in deploy script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
-export RESULT_DIR=admin.frontend.$TRAVIS_BUILD_NUMBER
-export SSHPASS=$SSH_PASS
+export RESULT_DIR=admin.frontend.${TRAVIS_BUILD_NUMBER}
+export SSHPASS=${SSH_PASS}
 export ARCH_NAME=frontend.package.tgz
 export SYMLINK_NAME=admin.frontend
 
-mkdir $RESULT_DIR
-mv -f ./build/* ./$RESULT_DIR
-tar -czf $ARCH_NAME $RESULT_DIR
-sshpass -e scp -C -o StrictHostKeyChecking=no $ARCH_NAME $SSH_USER@$SSH_IP:$WEB_PATH
-sshpass -e ssh -C $SSH_USER@$SSH_IP << EOF
-cd $WEB_PATH;
-tar -xzf ./$ARCH_NAME -C ./;
-rm ./$ARCH_NAME;
-rm -dRf $SYMLINK_NAME
-ln -ds $RESULT_DIR ./$SYMLINK_NAME
+mkdir ${RESULT_DIR}
+mv -f ./build/* ./${RESULT_DIR}
+tar -czf ${ARCH_NAME} ${RESULT_DIR}
+sshpass -e scp -C -o StrictHostKeyChecking=no ${ARCH_NAME} ${SSH_USER}@${SSH_IP}:${WEB_PATH}
+sshpass -e ssh -C ${SSH_USER}@${SSH_IP} << EOF
+cd ${WEB_PATH};
+tar -xzf ./${ARCH_NAME} -C ./;
+rm ./${ARCH_NAME};
+rm -dRf ${SYMLINK_NAME}
+ln -ds ${RESULT_DIR} ./${SYMLINK_NAME}
 EOF
